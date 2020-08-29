06efd530b38311e1d4d2fcc3561672be0c09f28c
Grace Frank <grace.frank@centricityinsights.com>
2020-08-29 21:06:13 +0100

    added paths

---
+++ src/config/paths.js
@@ -0,0 +1,6 @@
+export default {
+  LOGIN: "/login",
+  FLOWERS: "/flowers",
+  USERS: "/users",
+  NOT_FOUND: "/notfound",
+};
---
+++ src/config/urls.js
@@ -0,0 +1,3 @@
+const API_BASE_URL = "https://whispering-scrubland-26190.herokuapp.com/v1";
+export const API_USERS = `${API_BASE_URL}/users`;
+export const API_LOGIN = `${API_USERS}/login`;
