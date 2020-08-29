98d67d2717dd7043d93f456f805cbed673e8fc7c
Grace Frank <grace.frank@centricityinsights.com>
2020-08-29 21:06:30 +0100

    added loader

---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div>
+    <div className="spinner-grow text-info" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+    <div className="spinner-grow text-info" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+    <div className="spinner-grow text-info" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  </div>
+);
