2a463490f96846725a03dcc7263fdc668696ace6
Grace Frank <grace.frank@centricityinsights.com>
2020-09-06 20:05:19 +0100

    render two flowers per row

--- src/Pages/flowers/ActiveFlowers.js
+++ src/Pages/flowers/ActiveFlowers.js
@@ -1,28 +1,34 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import Sunburst from "./sunburst";
 import Spinner from "../../components/Spinner";
-import {FLOWERS} from '../../config/paths'
+import { FLOWERS } from "../../config/paths";
 
 const Flowers = ({ data, loading, viewFlowerDetails }) => {
   const flowers = data.map((item) => {
     return (
-      <div className="m-4" key={item._id}>
-        <Link to={`${FLOWERS}/${item._id}`}>
-        <button
-          type="button"
-          className="btn btn-info  btn-sm"
-        >
-          Details
-        </button>  
-        </Link>
+      <div className="col-lg-5 m-4 ml-10" key={item._id}>
         <Sunburst chartId={item._id} chartData={[item.flower]} />
-        
-            </div>
+        <Link to={`${FLOWERS}/${item._id}`}>
+          <button type="button" className="btn btn-info  btn-sm">
+            Details
+          </button>
+        </Link>
+      </div>
     );
   });
 
-  return <div className="">{!loading ? flowers : <div className="d-flex justifyContents-center"><Spinner /></div> }</div>;
+  return (
+    <div className="row">
+      {!loading ? (
+        flowers
+      ) : (
+        <div className="d-flex justifyContents-center">
+          <Spinner />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Flowers;
